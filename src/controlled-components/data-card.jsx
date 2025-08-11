18f3d7c9638db5f463b474b23d4bc7f2feb06db1
Suprit202 <supritmhnt123@gmail.com>
2025-08-12 03:41:09 +0530

    Enhance DataCard and ControlledDemo1 components with improved captions and layout for better user experience

--- src/controlled-components/data-card.jsx
+++ src/controlled-components/data-card.jsx
@@ -1,26 +1,36 @@
 export function DataCard(props) {
-  return(
-    <div className="d-flex flex-wrap">
-      {
-        props.data.map((item,i) => 
-          <div key={i} className="card mx-2" style={{width:'200px'}}>
-            <div className="card-header">
-              {
-                Object.keys(item)[0]
-              }
-            </div>
-            <div className="card-body">
-              {
-                item[Object.keys(item)[0]]
-              }
-            </div>
-            <div className="card-footer">
+  return (
+    <div>
+      <div className="my-3">{props.caption}</div>
+      
+      <div className="d-flex flex-wrap">
+        {
+          props.data.map((item, i) =>
+            <div key={i} className="card mx-2" style={{ width: '200px' }}>
+              <div className="card-header">
+                {
+                  <div className="bi bi-person-circle fs-5"> User Details</div>
+                }
+              </div>
+              <div className="card-body">
+                {
+                  Object.keys(item).map(key =>
+                    <div>
+                      <ul className="list-unstyled">
+                        <li><span className="fw-bold">{key}</span>: {item[key]}</li>
+                      </ul>
+                    </div>
+                  )
+                }
+              </div>
+              <div className="card-footer">
                 <button className="btn btn-warning bi bi-pen-fill"></button>
                 <button className="btn btn-danger bi bi-trash mx-3"></button>
+              </div>
             </div>
-          </div>
-        )
-      }
+          )
+        }
+      </div>
     </div>
   )
 }
--- src/controlledDemo1.jsx
+++ src/controlledDemo1.jsx
@@ -7,18 +7,18 @@ export function ControlledDemo1() {
   const [view,setView] = useState(<DataGrid1 caption='User details mention here' fields={['First Name','Last Name','age','Standard']} data={[{firstName:'David', lastName:'Brick', age:'23', std:'9th'}, {firstName:'Reno', lastName:'Leaf', age:'25', std:'10th'}]}></DataGrid1>);
 
   function GridView() {
-    setView(<DataGrid1 caption='User details mention here' fields={['First Name','Last Name','age','Standard']} data={[{firstName:'David', lastName:'Brick', age:'23', std:'9th'}, {firstName:'Reno', lastName:'Leaf', age:'25', std:'10th'}]}></DataGrid1>);
+    setView(<DataGrid1 caption='User details mention as table here' fields={['First Name','Last Name','age','Standard']} data={[{firstName:'David', lastName:'Brick', age:'23', std:'9th'}, {firstName:'Reno', lastName:'Leaf', age:'25', std:'10th'}]}></DataGrid1>);
   }
 
   function CardView() {
-    setView(<DataCard caption='User details mention here' fields={['First Name','Last Name','age','Standard']} data={[{firstName:'David', lastName:'Brick', age:'23', std:'9th'}, {firstName:'Reno', lastName:'Leaf', age:'25', std:'10th'}]}></DataCard>)
+    setView(<DataCard caption='User details mention as card here' fields={['First Name','Last Name','age','Standard']} data={[{firstName:'David', lastName:'Brick', age:'23', std:'9th'}, {firstName:'Reno', lastName:'Leaf', age:'25', std:'10th'}]}></DataCard>)
   }
 
   return(
     <div className="container-fluid p-3">
       <button className="btn btn-warning bi bi-pen-fill" onClick={GridView}>Grid View</button>
       <button className="btn btn-danger bi bi-trash mx-2" onClick={CardView}>Card View</button>
-      <h2>User details</h2>
+      <h2 className="mt-3">User details</h2>
       <div>{view}</div>
     </div>
   );
